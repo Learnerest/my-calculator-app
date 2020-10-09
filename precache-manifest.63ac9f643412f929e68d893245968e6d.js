4d97ed6ce3247003b48fb9a42cc3e286abe7d235
Fatih Kurt <fatihkurt@Fatih-MacBook-Air.local>
2020-10-09 17:01:05 +0300

    Updates

---
+++ precache-manifest.63ac9f643412f929e68d893245968e6d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fb3f2c436bdc60ecd93dcac99ec7d8d",
+    "url": "/my-calculator-app/index.html"
+  },
+  {
+    "revision": "bf1b56001b419ec6b548",
+    "url": "/my-calculator-app/static/css/main.c6e8d98c.chunk.css"
+  },
+  {
+    "revision": "cbd36766525ea1c69acf",
+    "url": "/my-calculator-app/static/js/2.bfeb77b4.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/my-calculator-app/static/js/2.bfeb77b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf1b56001b419ec6b548",
+    "url": "/my-calculator-app/static/js/main.d5780874.chunk.js"
+  },
+  {
+    "revision": "fe9539477f9114f23474",
+    "url": "/my-calculator-app/static/js/runtime-main.a21e1877.js"
+  }
+]);
